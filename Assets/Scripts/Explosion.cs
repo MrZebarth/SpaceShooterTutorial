1707744610a446a1e8e7c7dd6f275551851acd57
MrZebarth <kris.zebarth@ocsb.ca>
2018-07-26 21:34:58 -0400

    Spawn Manager

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(KillSwitch());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator KillSwitch()
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+    [SerializeField]
+    private GameObject enemyShipPrefab;
+    [SerializeField]
+    private GameObject[] powerUps;
+
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(EnemySpawn());
+        StartCoroutine(PowerUpSpawn());
+	}
+	
+	IEnumerator EnemySpawn() 
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5.0f);
+            Instantiate(enemyShipPrefab, new Vector3(Random.Range(-8f, 8f), 8f, 0), Quaternion.identity);
+        }
+    }
+
+    IEnumerator PowerUpSpawn()
+    {
+        while (true)
+        {
+            int randomPowerup = Random.Range(0, 3);
+            yield return new WaitForSeconds(8.0f);
+            Instantiate(powerUps[randomPowerup], new Vector3(Random.Range(-8f, 8f), 8f, 0), Quaternion.identity);
+        }
+    }
+
+}
